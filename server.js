5ebba081a5ec4d0e1ae3dc23bf065104c61171c4
CaseyKoppes <casey.koppes@gmail.com>
2016-01-29 15:59:47 -0800

    updated files

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var http = require('http');
+http = require("http")
+cfenv = require("cfenv")
 
-http.createServer(function(request, response) 
+appEnv   = cfenv.getAppEnv()
+instances = appEnv.app.instance_index || 0
+
+
+
+// create a server with a simple request handler
+server = http.createServer(onRequest)
+
+// start the server on the calculated port and host
+server.listen(appEnv.port, function() {
+    console.log("server starting on " + appEnv.url)
+})
+
+
+
+//-----------------------------------------------
+function onRequest(request, response) 
+
 { response.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><body><h1>Hello, World</h1></body></html>');
-}).listen(process.env.VCAP_APP_PORT);
+  response.end('<html><body><h1>Hello, World!</h1></body></html>');
+}
